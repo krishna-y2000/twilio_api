5d372ae569f39adf36c4da06372e3b36d38679fa
Krishnaraj Yadav <krishnay.75676@gmail.com>
2021-01-13 18:57:05 +0530

    commit

--- index.js
+++ index.js
@@ -1,38 +1,40 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
-dotenv.config( { path : "./config.env" })
+dotenv.config( { path : "/home/krishnaraj/Desktop/twilio_api/.env" })
  const port = process.env.port || 3000 ;
 const accountSid = 'AC231da50ba9eb6747db0090cea4ac3396'; 
 const authToken =  process.env.authToken; 
 var twilio = require('twilio');
 var client = new twilio(accountSid, authToken);
 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
+console.log(authToken);
+app.get('/login/verified' ,async (req,res) => {
+ // res.render("home.ejs");
 
-app.get('/login' ,(req,res) => {
-  res.render("home.ejs");
+  await client.verify.services.create({friendlyName: 'Mysuccessfulapp'})
+ .then(service =>  {
+      client.verify.services(service.sid)
+     .verifications
+     .create({to: `+${req.query.phonenumber}`, channel: req.query.channel })
+     .then(verification => res.status(200).send(verification.status))
+     .catch( (err) => console.log(err))
+
+     
+ })
+ .catch((err) => console.log(err) )
 } )
 
-app.post('/login/verified' ,async (req,res) => {
-    console.log(req.body.channelvalue );
-    console.log( req.body.phonenumbervalue);
-    const serviceID = await client.verify.services.create({friendlyName: 'Mysuccessfulapp'})
-    .then(service =>  {
-         client.verify.services(service.sid)
-        .verifications
-        .create({to: `+${req.body.phonenumbervalue}`, channel: req.body.channelvalue })
-        .then(verification => res.status(200).send(verification.status))
-        .catch( (err) => console.log(err))
-
-        
-    })
-    .catch((err) => console.log(err) )
+// app.post('/login/verified' ,async (req,res) => {
+//     console.log(req.body.channelvalue );
+//     console.log( req.body.phonenumbervalue);
+  
    
 
 
-}) ;
+// }) ;
 
 
 
